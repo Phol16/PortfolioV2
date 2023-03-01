c494e231353c688a9f907c30c01a5cc7a090e001
Phol <Pholibertlim@gmail.com>
2023-03-01 19:08:50 +0800

    fix: fixed info and codes

--- src/components/MainAbout.jsx
+++ src/components/MainAbout.jsx
@@ -66,34 +66,54 @@ const techStack = [
   },
 ];
 
+const Educ = [
+  {
+    title: 'Uplift Code Camp',
+    info: 'Full Stack Development',
+    year: ['2022'],
+  },
+  {
+    title: 'Ateneo de Zamboanga University',
+    info: 'Bachelor of Science in Computer Engineering',
+    year: ['2015', '2022'],
+  },
+  {
+    title:'Technical Education and Skills Development Authority',
+    info:'NC II - Computer System Servicing',
+    year:['2019']
+  }
+];
+
 const MainAbout = () => {
   return (
     <div className='py-10 px-2 flex flex-col sm:items-center gap-5' id='About'>
       <main>
         <h1 className='text-sky-400 text-xl xl:text-wxl'>About Me</h1>
-        <p className='p-1 text-sm md:max-w-lg xl:text-base xl:max-w-xl'>I am an aspiring software developer with experience using the MERN stack for web development. While I am not a seasoned professional, I am knowledgeable on how to use MongoDB, ExpressJS, ReactJs, and NodeJS to build dynamic and engaging web applications.</p>
+        <p className='p-1 text-sm md:max-w-lg xl:text-base xl:max-w-xl 2xl:max-w-3xl'>I am an aspiring software developer with experience using the MERN stack for web development. While I am not a seasoned professional, I am knowledgeable on how to use MongoDB, ExpressJS, ReactJs, and NodeJS to build dynamic and engaging web applications.</p>
       </main>
       <section className='grid sm:grid-cols-2 gap-5 md:gap-10 items-center '>
-        <div className='p-1'>
+        <div className='p-1 gap-4 flex flex-col'>
           <h2 className='text-sky-400 text-lg xl:text-xl'>Education</h2>
-          <h3>Ateneo de Zamboanga University</h3>
-          <p className='p-1 text-sm xl:text-base'>
-            LaPurisima St., Zamboanga City <br />
-            Bachelor of Science in Computer Engineering <br />
-            June 2015 - March 2021
-          </p>
-          <h2 className='text-sky-400 text-lg xl:text-xl mt-4'>Training</h2>
-          <h3>Uplift CodeCamp</h3>
-          <p className='p-1 text-sm xl:text-base'>
-            <span className='text-sky-200'>MERN Stack</span> <br /> July 16, 2022 - Dec 23, 2022
-          </p>
+          {Educ.map((details) => {
+            return (
+              <div>
+                <h3 className='text-sky-300'>{details.title}</h3>
+                <p className='p-1 text-sm xl:text-base '>
+                  {details.info} / {' '}
+                  {details.year.map((e, index) => {
+                    return index !== details.year.length -1 ? <span>{e} - </span>: <span>{e}</span>;
+                  })}
+                </p>
+              </div>
+            );
+          })}
         </div>
-        <div className='md:justify-self-center'>
+        <div className='md:justify-self-end'>
           <h2 className='text-sky-400 text-lg xl:text-xl'>Random Facts</h2>
-          <ul className='text-sm p-2 xl:text-base'>
-            <li>I lived in the southern Philippines</li>
-            <li>Favorite colors are black and white </li>
-            <li>Silver Rank in Valorant </li>
+          <ul className='text-sm p-2 xl:text-base flex flex-col gap-2'>
+            <li> I lived in the southern Philippines</li>
+            <li> Favorite colors are black and white </li>
+            <li> Silver Rank in Valorant </li>
           </ul>
         </div>
         <section className='sm:col-span-2 sm:justify-self-center'>
