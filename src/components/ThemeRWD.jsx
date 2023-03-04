9ac9cdeac66e6b06f1347e95242d3dfad7a88ae0
Phol <Pholibertlim@gmail.com>
2023-03-04 15:35:47 +0800

    fixed fonts

--- src/components/ThemeRWD.jsx
+++ src/components/ThemeRWD.jsx
@@ -1,7 +1,7 @@
 export const Theading = 'text-lg md:text-xl xl:text-2xl font-bold';
-export const TsubHeading = 'text-base md:text-lg xl:text-xl font-medium';
+export const TsubHeading = 'text-sm md:text-base xl:text-lg font-medium';
 export const TminiHeading = 'text-xs md:text-sm xl:text-base font-medium';
-export const TminiContent = 'text-xs xl:text-sm p-1 font-normal';
+export const TminiContent = 'text-xs xl:text-sm p-1 font-medium';
 
 export const FASizeIcon = 'text-xl xl:text-2xl p-1';
 export const SVGSizeWH = 'w-10 p-1';
