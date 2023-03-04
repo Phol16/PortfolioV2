eb3a5c2b551a3ecafa6bf9a94f62be2d2eb8f116
Phol <Pholibertlim@gmail.com>
2023-03-05 03:04:19 +0800

    fixed some format

---
+++ src/components/ThemeFormat.jsx
@@ -0,0 +1,15 @@
+export const ThemeFormat = {
+  primaryBGDark: 'bg-zinc-800',
+  primaryBGLight: 'bg-slate-200',
+
+  secondaryBGDark: 'bg-sky-800 duration-[500ms]',
+
+  primaryDColorText: 'text-white',
+
+  secondaryDColorText: 'text-sky-400',
+  tertiaryDColorText: 'text-sky-200',
+
+  DMain: 'font-bold text-2xl md:text-4xl',
+  DSub:'font-semibold text-sm md:text-lg',
+  DMini:'font-medium text-xs md:text-base'
+}
