30e3c1e66cb09beed0e73a5f6e4331ca0b1ffe59
Phol <Pholibertlim@gmail.com>
2023-03-05 17:43:34 +0800

    fixed something in aboutme section

--- src/components/MainAbout.jsx
+++ src/components/MainAbout.jsx
@@ -3,6 +3,7 @@ import SkillGroup from './SkillGroup';
 import { design, programmingLanguage, repo, skills, techStack } from './SkillData';
 import { Theading, TsubHeading, TminiHeading, TminiContent, secondaryTextColor, tertiaryTextColor, primaryTextColor, primaryTextColorDark, secondaryTextColorDark, tertiaryTextColorDark } from './ThemeRWD';
 import { ThemeContext } from '../layout/Intro';
+import { ThemeFormat } from './ThemeFormat';
 
 const Educ = [
   {
@@ -47,43 +48,18 @@ const groupSkill = [
 
 const MainAbout = () => {
   const { dark } = useContext(ThemeContext);
+  const {Theading, primaryDColorText, primaryLColorText,secondaryDColorText, secondaryLColorText} = ThemeFormat
   const PTextColor = dark ? primaryTextColor : primaryTextColorDark;
   const STextColor = dark ? secondaryTextColor : secondaryTextColorDark;
   const TTextColor = dark ? tertiaryTextColor : tertiaryTextColorDark;
 
   return (
-    <div className={`${PTextColor} py-10 px-2 flex flex-col m-auto gap-5 max-w-md md:max-w-xl xl:max-w-7xl`} id='About'>
+    <div className={`${dark ? primaryDColorText : primaryLColorText} flex flex-col py-10 max-w-fit m-auto gap-10`} id='About'>
       <main className='max-w-4xl m-auto'>
         <h1 className={`${Theading} ${STextColor}`}>About Me</h1>
-        <p className={`${TsubHeading} m-auto p-2 `}>I am an aspiring software developer with experience using the MERN stack for web development. While I am not a seasoned professional, I am knowledgeable on how to use MongoDB, Express JS, React Js, and Node JS to build dynamic and engaging web applications.</p>
+        <p className={`${TsubHeading} m-auto p-2 `}>I am an aspiring software developer with experience using the MERN stack for web development. While I am not a seasoned professional, I am knowledgeable on how to use MongoDB, Express JS, React Js, and Node JS to build dynamic and engaging web applications. I am also eager to learn other technologies as well.</p>
       </main>
       <section className='flex flex-col justify-center items-center gap-5'>
-        <article className='grid md:grid-cols-2 gap-5 md:gap-10 p-2'>
-        <div className=' gap-4 flex flex-col max-w-sm '>
-          <h2 className={`${TsubHeading} ${STextColor}`}>Education</h2>
-          {Educ.map((details) => {
-            return (
-              <div key={details.title}>
-                <h3 className={`${TminiHeading} ${TTextColor}`}>{details.title}</h3>
-                <p className={TminiContent}>
-                  {details.info} <br />/{' '}
-                  {details.year.map((e, index) => {
-                    return index !== details.year.length - 1 ? <span key={e}>{e} - </span> : <span key={e}>{e}</span>;
-                  })}
-                </p>
-              </div>
-            );
-          })}
-        </div>
-      <div className='md:self-center md:justify-self-center'>
-          <h2 className={`${TsubHeading} ${STextColor}`}>Random Facts</h2>
-          <ul className={`${TminiContent} flex flex-col gap-2`}>
-            <li> I lived in the southern Philippines</li>
-            <li> Favorite colors are black and white </li>
-            <li> Silver Rank in Valorant </li>
-          </ul>
-        </div>
-        </article>
         <main className=''>
           <h2 className={`${TsubHeading} ${STextColor} xl:px-20`}>Skills</h2>
           <section className='grid md:grid-cols-2 xl:grid-cols-3 gap-4'>
