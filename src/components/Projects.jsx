325bcac52afa497184376fb649cedb62c01a064c
Phol <Pholibertlim@gmail.com>
2023-03-12 16:29:40 +0800

    arranged some stuff

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -25,26 +25,6 @@ const Information = [
     source: 'https://github.com/Phol16',
   },
   {
-    title: 'Personal Portfolio',
-    tech: 'ReactJS HTML CSS JS TailwindCSS',
-    description: `A Web-Based application developed by using ReactJS.
-    The current website is this application.
-    It is an application that shows the details and the projects`,
-    image: 'https://res.cloudinary.com/df8nkgje6/image/upload/v1678117992/Portfolio/portfolio0_wcn5y2.png',
-    demo: 'https://phol.vercel.app/',
-    source: 'https://github.com/Phol16/PortfolioV2',
-  },
-  {
-    title: 'The Maze Game',
-    tech: 'HTML CSS JS',
-    description: `A simple application developed by using HTML CSS and JS.
-    It is an application where you can choose game difficulty.
-    It is an application develop for pc because the user will use the keyboard to play`,
-    image: 'https://res.cloudinary.com/df8nkgje6/image/upload/v1678089830/Portfolio/mazegame0_cdq58a.png',
-    demo: 'https://phol16.github.io/TheMazeGame/startingPage.html',
-    source: 'https://github.com/Phol16/TheMazeGame',
-  },
-  {
     title: 'Pokemon Pokedex',
     tech: 'ReactJS RestfulAPI MUI',
     description: `A Web-Based application developed by using ReactJS.
@@ -76,7 +56,7 @@ const Projects = () => {
   return (
     <div className={`${PTextColor} flex flex-col max-w-fit m-auto gap-10`} id='Projects'>
       <h1 className={`${Theading} ${STextColor} p-2`}>Projects</h1>
-      <main className={`grid gap-5 justify-center items-center lg:grid-cols-2 gap-5 w-fit self-center overflow-hidden py-10`}>
+      <main className={`grid gap-5 justify-center items-center xl:grid-cols-2 gap-5 w-fit self-center overflow-hidden py-10`}>
         {Information.map((Details, index) => {
           return <ProjectDetails number={index + 1} key={Details.title} title={Details.title} tech={Details.tech} description={Details.description} image={Details.image} demo={Details.demo} source={Details.source} />;
         })}
