cf8eac89d3fa05f7dd4ac1736c1d41297712b5dc
Phol <Pholibertlim@gmail.com>
2023-03-15 19:37:33 +0800

    updated photo

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -50,7 +50,7 @@ const Information = [
     description: `A Web-Based application developed by using ReactJS.
     The current website is this application.
     It is an application that shows the details and the projects`,
-    image: 'https://res.cloudinary.com/df8nkgje6/image/upload/v1678117992/Portfolio/portfolio0_wcn5y2.png',
+    image: 'https://res.cloudinary.com/df8nkgje6/image/upload/v1678880213/Portfolio/portfolio_jmaefg.png',
     demo: 'https://phol.vercel.app/',
     source: 'https://github.com/Phol16/PortfolioV2',
   },
