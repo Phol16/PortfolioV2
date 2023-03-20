43f1a1cbe918fafe667b86b30dd830d5868bc8f0
Phol <Pholibertlim@gmail.com>
2023-03-20 23:33:26 +0800

    fixed some sentence/words/stirngs

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,7 +7,7 @@ const Information = [
   {
     title: 'MovieNotepad',
     tech: 'MongoDb ExpressJS NodeJS ReactJS',
-    description: `A fullstack application developed by using MERN stack. 
+    description: `A fullstack application developed using MERN stack. 
     It is an application where user can add movie or series by providing details/information about a specific movie. 
     It is also an application where user can user can add movies to their watchlist and add notes to a specific movie in the watchlist.`,
     image: 'https://res.cloudinary.com/df8nkgje6/image/upload/v1678977547/Portfolio/Untitled_nsv7an.png',
@@ -17,7 +17,7 @@ const Information = [
   {
     title: 'Mini Messenger',
     tech: 'MongoDb ExpressJS SocketIO ReactJs',
-    description: `An simple application that is almost a like from messenger where user can communicate with one another, it is a very simple application that uses socket io for realtime communication`,
+    description: `A fullstack application developed using MERN stack with socketIO. It is a simple application that almost look like messenger where user can communicate with one another, it is a very simple application that uses socket io for realtime communication`,
     image: 'https://res.cloudinary.com/dy23rmhmq/image/upload/v1679321037/Portfolio/Untitled_b3kuhi.png',
     demo: 'https://minimessenger.onrender.com/',
     source: 'https://github.com/Phol16/ChatApp',
@@ -25,7 +25,7 @@ const Information = [
   {
     title: 'Personal Portfolio',
     tech: 'ReactJS HTML CSS JS TailwindCSS',
-    description: `A Web-Based application developed by using ReactJS.
+    description: `A Web-Based application developed using ReactJS.
     The current website is this application.
     It is an application that shows the details and the projects`,
     image: 'https://res.cloudinary.com/df8nkgje6/image/upload/v1678880213/Portfolio/portfolio_jmaefg.png',
@@ -35,7 +35,7 @@ const Information = [
   {
     title: 'Eatstagram',
     tech: 'MongoDB ExpressJS ReactJS NodeJS Auth0',
-    description: `A fullstack application developed by using MERN stack.
+    description: `A fullstack application developed using MERN stack.
     It is an application developed by UpliftCodeCamp Batch12.
     It is an application where you can upload photos of your favorite foods, you can interact to other photos that was uploaded by other users as well.`,
     image: 'https://res.cloudinary.com/df8nkgje6/image/upload/v1678191964/Portfolio/eatstagram0_ryyp3i.png',
@@ -45,7 +45,7 @@ const Information = [
   {
     title: 'Pokemon Pokedex',
     tech: 'ReactJS RestfulAPI MUI',
-    description: `A Web-Based application developed by using ReactJS.
+    description: `A Web-Based application developed using ReactJS.
     It is an application the that display a list of pokemons.
     It has a feature where you can capture the pokemon.`,
     image: 'https://res.cloudinary.com/df8nkgje6/image/upload/v1678262045/Portfolio/Untitled_qxjfwr.png',
@@ -55,7 +55,7 @@ const Information = [
   {
     title: 'Weather App',
     tech: 'ReactJS RestfulAPI ',
-    description: `A Web-Based application developed by using ReactJS.
+    description: `A Web-Based application developed using ReactJS.
     It is an application where it shows information about the weather in zamboanga city.
     It is a simple application.`,
     image: 'https://res.cloudinary.com/df8nkgje6/image/upload/v1678193519/Portfolio/weatherApp0_drppyc.png',
