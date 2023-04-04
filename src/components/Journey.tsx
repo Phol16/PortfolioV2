e7b126f8c9e170dfc793e6e7ae53da096ec349f3
Phol <Pholibertlim@gmail.com>
2023-04-04 23:37:06 +0800

    to update vercel

--- src/components/Journey.tsx
+++ src/components/Journey.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Journey = () => {
   return (
-    <div className=' text-secondaryTextColor flex flex-col py-10'>
+    <div className=' text-primaryTextColor flex flex-col py-10'>
       <div className='my-10'>Journey</div>
       <p className='self-center'>Currently Developing</p>
     </div>
