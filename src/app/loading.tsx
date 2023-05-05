dccb8f0f172b5a2c36f95e8028cbdb095384f310
Phol <Pholibertlim@gmail.com>
2023-05-05 22:39:51 +0800

    changed loading function name

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Loading = () => {
+const loading = () => {
   return (
     <div className='text-black bg-primaryColorL w-full h-[calc(100%_-_92px)] flex justify-center items-center text-6xl'>Loading...</div>
   )
 }
 
-export default Loading
+export default loading
