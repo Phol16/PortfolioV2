743a00bbe9c70f6f421e8a8bc897d4f33ae9750c
Phol <Pholibertlim@gmail.com>
2023-05-07 00:55:03 +0800

    git stats

--- src/app/aboutPage/page.tsx
+++ src/app/aboutPage/page.tsx
@@ -134,7 +134,7 @@ const AboutPage = () => {
           className='h-auto'
         />
         <Image
-          src=' https://github-readme-stats.vercel.app/api/top-langs/?username=phol16&hide_progress=true&theme=graywhite'
+          src='https://github-readme-stats.vercel.app/api/top-langs/?username=phol16&hide_progress=true'
           alt='Gstatus'
           width={400}
           height={0}
